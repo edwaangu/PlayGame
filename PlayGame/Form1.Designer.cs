e4d3c6243ae0f7ed1ac3da6f91f3714e1dcef3f5
edwaangu <edwaangu171@outlook.com>
2021-04-23 09:43:23 -0400

    Add project files.

---
+++ PlayGame/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿
+namespace PlayGame
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.PlayButton = new System.Windows.Forms.Button();
+            this.OutputText = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // PlayButton
+            // 
+            this.PlayButton.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.PlayButton.Font = new System.Drawing.Font("Arial", 14F, System.Drawing.FontStyle.Bold);
+            this.PlayButton.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.PlayButton.Location = new System.Drawing.Point(110, 140);
+            this.PlayButton.Name = "PlayButton";
+            this.PlayButton.Size = new System.Drawing.Size(160, 70);
+            this.PlayButton.TabIndex = 0;
+            this.PlayButton.Text = "Play the game";
+            this.PlayButton.UseVisualStyleBackColor = false;
+            this.PlayButton.Click += new System.EventHandler(this.PlayButton_Click);
+            // 
+            // OutputText
+            // 
+            this.OutputText.BackColor = System.Drawing.Color.Transparent;
+            this.OutputText.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.OutputText.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.OutputText.Location = new System.Drawing.Point(110, 150);
+            this.OutputText.Name = "OutputText";
+            this.OutputText.Size = new System.Drawing.Size(160, 70);
+            this.OutputText.TabIndex = 1;
+            this.OutputText.Text = "Starting in 3...";
+            this.OutputText.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(384, 361);
+            this.Controls.Add(this.OutputText);
+            this.Controls.Add(this.PlayButton);
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Great Playable Game";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button PlayButton;
+        private System.Windows.Forms.Label OutputText;
+    }
+}
+
---
+++ PlayGame/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.Media;
+
+namespace PlayGame
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            OutputText.Hide();
+        }
+
+        private void PlayButton_Click(object sender, EventArgs e)
+        {
+            SoundPlayer tickPlayer = new SoundPlayer(Properties.Resources.Tick);
+            SoundPlayer goPlayer = new SoundPlayer(Properties.Resources.Go_Sound);
+            PlayButton.Hide();
+            OutputText.Show();
+            tickPlayer.Play();
+
+            Refresh();
+            Thread.Sleep(1000);
+            OutputText.Text = "Starting in 2...";
+            tickPlayer.Play();
+
+            Refresh();
+            Thread.Sleep(1000);
+            OutputText.Text = "Starting in 1...";
+            tickPlayer.Play();
+
+            Refresh();
+            Thread.Sleep(1000);
+            this.BackColor = Color.LimeGreen;
+            OutputText.Text = "Go!";
+            goPlayer.Play();
+        }
+    }
+}
